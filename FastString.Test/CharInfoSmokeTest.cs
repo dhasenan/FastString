795e20fc7c671fb359206590afed66c6297212f8
dhasenan <dhasenan@gmail.com>
2017-04-14 08:53:34 -0700

    Use resources to get unicode data.
    Helps with #1.

--- FastString.Test/CharInfoSmokeTest.cs
+++ FastString.Test/CharInfoSmokeTest.cs
@@ -7,15 +7,34 @@ namespace FastString.Test
 	[TestFixture]
 	public class CharInfoSmokeTest
 	{
-		[Test]
-		public void IndexCodepoint()
+		[SetUp]
+		public void Setup()
 		{
-			/*
-			for (int i = 0; i < CharInfo.Characters.Count; i++)
-			{
-				Assert.That(CharInfo.Characters[i].Codepoint, Is.EqualTo((uint)i));
-			}
-*/
+			CharInfo.LoadCharacterData();
+			CharInfo.LoadCharacterNames();
+		}
+
+		[Test]
+		public void AsciiTab()
+		{
+			var ci = CharInfo.For('\t').Value;
+			Assert.That(ci.Name, Is.EqualTo(new utf8("<control>")), ci.Name.ToString());
+		}
+
+		[Test]
+		public void AsciiDollar()
+		{
+			var ci = CharInfo.For('$').Value;
+			Assert.That(ci.Codepoint, Is.EqualTo((uint)0x24));
+			Assert.That(ci.Name, Is.EqualTo(new utf8("DOLLAR SIGN")), ci.Name.ToString());
+			Assert.That(ci.Category, Is.EqualTo(UnicodeCategory.SymbolCurrency));
+		}
+
+		[Test]
+		public void NearTop()
+		{
+			Assert.NotNull(CharInfo.For(0x100000));
+			Assert.NotNull(CharInfo.For(0x10FFFD));
 		}
 	}
 }
--- FastString/Unicode/CharInfo.cs
+++ FastString/Unicode/CharInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 namespace FastString.Unicode
 {
 	public enum UnicodeCategory
@@ -35,8 +39,162 @@ namespace FastString.Unicode
 		SymbolOther,
 	}
 
-	public partial struct CharInfo
+	[StructLayout(LayoutKind.Explicit, Pack = 1)]
+	struct FloatBytes
 	{
+		[FieldOffset(0)]
+		public float f;
+		[FieldOffset(0)]
+		public uint i;
+	}
+
+	public struct CharInfo
+	{
+		private const int _serializedLength = 30;
+		public static CharInfo? For(uint codepoint)
+		{
+			// We have 30k-ish codepoints.
+			// They're scattered across a much larger range, but at the earliest codepoint X is in position X.
+			// There are a fair bit glommed together at the start, though.
+			if (Match(codepoint * _serializedLength, codepoint))
+			{
+				return LoadAtOffset(codepoint * _serializedLength);
+			}
+
+			// It is not lowerBarrier and is not lower than it.
+			int lowerBarrier = -1;
+			// It is not upperBarrier and is not greater than it.
+			int upperBarrier = _rawData.Length / _serializedLength;
+			Console.WriteLine("start search for {2}: lower {0} upper {1}", lowerBarrier, upperBarrier, codepoint);
+
+			while (lowerBarrier < upperBarrier - 1)
+			{
+				var guess = (upperBarrier - lowerBarrier) / 2 + lowerBarrier;
+				var it = ReadUint32(guess * _serializedLength);
+				if (it == codepoint)
+				{
+					return LoadAtOffset(guess * _serializedLength);
+				}
+				if (it < codepoint)
+				{
+					lowerBarrier = guess;
+				}
+				if (it > codepoint)
+				{
+					upperBarrier = guess;
+				}
+				Console.WriteLine("guess: {0} was: {1} lower: {2} upper: {3}", guess, it, lowerBarrier, upperBarrier);
+			}
+
+			return null;
+		}
+
+		static CharInfo LoadAtOffset(long offset)
+		{
+			CharInfo info = new CharInfo();
+
+			info.Codepoint = ReadUint32(offset);
+			offset += 4;
+
+			var nameStart = ReadUint32(offset);
+			offset += 4;
+
+			var nameEnd = ReadUint32(offset);
+			offset += 4;
+
+			if (_rawNames != null)
+			{
+				info.Name = new utf8(new ArraySegment<byte>(_rawNames, (int)nameStart, (int)(nameEnd - nameStart)));
+			}
+
+			info.Category = (UnicodeCategory)_rawData[offset];
+			offset++;
+
+			var fb = new FloatBytes();
+			fb.i = ReadUint32(offset);
+			info.NumericValue = fb.f;
+			offset += 4;
+
+			info.Mirrored = _rawData[offset] == 1;
+			offset++;
+
+			info.Uppercase = ReadUint32(offset);
+			offset += 4;
+
+			info.Lowercase = ReadUint32(offset);
+			offset += 4;
+
+			info.Titlecase = ReadUint32(offset);
+
+			return info;
+		}
+
+		static bool Match(long offset, uint codepoint)
+		{
+			if (offset < 0 || offset >= _rawData.Length - _serializedLength)
+			{
+				return false;
+			}
+			var at = ReadUint32(offset);
+			return at == codepoint;
+		}
+
+		static uint ReadUint32(long offset)
+		{
+			uint val = 0;
+			val <<= 8; val |= _rawData[offset];
+			val <<= 8; val |= _rawData[offset + 1];
+			val <<= 8; val |= _rawData[offset + 2];
+			val <<= 8; val |= _rawData[offset + 3];
+			return val;
+		}
+
+		public static void LoadCharacterData()
+		{
+			if (_rawData != null)
+			{
+				return;
+			}
+			lock (_lock)
+			{
+				if (_rawData != null)
+				{
+					return;
+				}
+				var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FastString.Unicode.chardata");
+				using (var mem = new MemoryStream())
+				{
+					stream.CopyTo(mem);
+					_rawData = mem.ToArray();
+				}
+			}
+		}
+
+		public static void LoadCharacterNames()
+		{
+			if (_rawNames != null)
+			{
+				return;
+			}
+			lock (_lock)
+			{
+				if (_rawNames != null)
+				{
+					return;
+				}
+				var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FastString.Unicode.charnames");
+				using (var mem = new MemoryStream())
+				{
+					stream.CopyTo(mem);
+					_rawNames = mem.ToArray();
+				}
+			}
+		}
+
+		private static object _lock = new object();
+		private static byte[] _rawData;
+		private static byte[] _rawNames;
+
 		public static UnicodeCategory ParseCategory(utf8 str)
 		{
 			if (str == "Lu") return UnicodeCategory.LetterUppercase;
@@ -80,28 +238,58 @@ namespace FastString.Unicode
 		/// <summary>
 		/// If this is representable as a single UTF-16 code unit, this contains that code unit.
 		/// </summary>
-		public char? CharValue { get; internal set; }
+		public char? CharValue
+		{
+			get
+			{
+				if (Codepoint <= 0xFFFF)
+				{
+					return (char)Codepoint;
+				}
+				return null;
+			}
+		}
 
 		/// <summary>
 		/// If this is represented as two UTF-16 code units, the high surrogate contains the high order half.
 		/// </summary>
-		public char? SurrogateHighValue { get; internal set; }
+		public char? SurrogateHighValue
+		{
+			get
+			{
+				if (Codepoint <= 0xFFFF)
+				{
+					return null;
+				}
+				return (char)((Codepoint - 0x10000) >> 10);
+			}
+		}
 
 		/// <summary>
 		/// If this is represented as two UTF-16 code units, the low surrogate contains the low order half.
 		/// </summary>
-		public char? SurrogateLowValue { get; internal set; }
+		public char? SurrogateLowValue
+		{
+			get
+			{
+				if (Codepoint <= 0xFFFF)
+				{
+					return null;
+				}
+				return (char)((Codepoint - 0x10000) & 0x3FF);
+			}
+		}
 
 		/// <summary>
 		/// The human-readable name of this codepoint, such as "LATIN CAPITAL LETTER A".
 		/// </summary>
 		public utf8 Name { get; internal set; }
 
+		public UnicodeCategory Category { get; internal set; }
+		public bool Mirrored { get; internal set; }
 		public double NumericValue { get; internal set; }
 		public uint Uppercase { get; internal set; }
 		public uint Lowercase { get; internal set; }
 		public uint Titlecase { get; internal set; }
-		public UnicodeCategory Category { get; internal set; }
-		public bool Mirrored { get; internal set; }
 	}
 }
